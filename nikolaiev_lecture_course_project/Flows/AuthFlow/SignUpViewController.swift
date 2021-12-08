1a010431d3d60dff4745d1f7a756302ef49ba028
Niko-SV <sergei93nikolaiev@gmail.com>
2021-12-08 20:09:23 +0200

    Lesson 11 draft

--- nikolaiev_lecture_course_project/Flows/AuthFlow/SignUpViewController.swift
+++ nikolaiev_lecture_course_project/Flows/AuthFlow/SignUpViewController.swift
@@ -6,14 +6,68 @@
 //
 import UIKit
 
-class SignUpViewController: UIViewController {
+class SignUpViewController: UIViewController, UITextFieldDelegate {
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-    }
+    @IBOutlet weak var scrollView: UIScrollView!
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var confirmPassword: UITextField!
     
     @IBAction func signUpButton(_ sender: UIButton) {
         UserDefaults.standard.set(true, forKey: DefaultKeys.userLoggedIn)
+    }
+    
+    weak var activeField: UITextField?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        NotificationCenter.default.addObserver(self, selector: #selector(SignUpViewController.keyboardDidShow),
+                                               name: UIResponder.keyboardDidShowNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(SignUpViewController.keyboardWillBeHidden),
+                                               name: UIResponder.keyboardWillHideNotification, object: nil)
+        
+    }
+    
+    private func hideKeyboardWhenTappedAround() {
+        let tap = UITapGestureRecognizer(target: self, action: #selector(self.dismissKeyboard))
+        self.view.addGestureRecognizer(tap)
+    }
+    
+    @objc func dismissKeyboard() {
+        view.endEditing(true)
+    }
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        if let placeholder = textField.placeholder {
+            UserDefaults.standard.set(textField.text, forKey: placeholder)
+        }
+        activeField = nil
+    }
+    
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+        activeField = textField
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+    
+    @objc func keyboardDidShow(notification: Notification) {
+        let keyboardSize = (notification.userInfo?[UIResponder.keyboardFrameBeginUserInfoKey] as? NSValue)?.cgRectValue
+        guard let activeField = activeField, let keyboardHeight = keyboardSize?.height else { return }
+        
+        let contentInsets = UIEdgeInsets(top: 0.0, left: 0.0, bottom: keyboardHeight, right: 0.0)
+        scrollView.contentInset = contentInsets
+        scrollView.scrollIndicatorInsets = contentInsets
+        let activeRect = activeField.convert(activeField.bounds, to: scrollView)
+        scrollView.scrollRectToVisible(activeRect, animated: true)
+    }
+    
+    @objc func keyboardWillBeHidden(notification: Notification) {
+        let contentInsets = UIEdgeInsets.zero
+        scrollView.contentInset = contentInsets
+        scrollView.scrollIndicatorInsets = contentInsets
     }
     
 }
